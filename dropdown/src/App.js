e88f3d4e061bcadd4f2560bf27937278a7dd4cd3
faruk-gkc <65074484+faruk-gkc@users.noreply.github.com>
2021-02-23 18:20:48 +0300

    React App
    React Basic App

---
+++ dropdown/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import ContactUs from './components/pages/ContactUs';
+import SignUp from './components/pages/SignUp';
+import Marketing from './components/pages/Marketing';
+import Consulting from './components/pages/Consulting';
+import Design from './components/pages/Design';
+import Development from './components/pages/Development';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/services' component={Services} />
+        <Route path='/products' component={Products} />
+        <Route path='/contact-us' component={ContactUs} />
+        <Route path='/sign-up' component={SignUp} />
+        <Route path='/marketing' component={Marketing} />
+        <Route path='/consulting' component={Consulting} />
+        <Route path='/design' component={Design} />
+        <Route path='/development' component={Development} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ dropdown/src/components/pages/Development.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Design() {
+  return (
+    <>
+      <h1 className='development'>DEVELOPMENT</h1>
+    </>
+  );
+}
